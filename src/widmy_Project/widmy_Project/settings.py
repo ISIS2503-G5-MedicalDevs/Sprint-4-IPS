56a5b7f6675c8464d8bf9d5b5ac8f16a2b2a88ae
msgalvan1207 <85647232+msgalvan1207@users.noreply.github.com>
2023-05-06 00:44:15 -0500

    Autenticacion correcta

--- src/widmy_Project/widmy_Project/settings.py
+++ src/widmy_Project/widmy_Project/settings.py
@@ -154,6 +154,6 @@ SOCIAL_AUTH_AUTH0_SCOPE = [
     'role', 
     ] 
 AUTHENTICATION_BACKENDS = { 
-    'monitoring.auth0backend.Auth0', 
+    'widmy_Project.auth0backend.Auth0', 
     'django.contrib.auth.backends.ModelBackend', 
     }
