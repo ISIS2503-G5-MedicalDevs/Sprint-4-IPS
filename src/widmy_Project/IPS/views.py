d248e083a8e141c516f953f38b3c79cad8f5552e
msgalvan1207 <85647232+msgalvan1207@users.noreply.github.com>
2023-05-06 00:36:36 -0500

    Views para IPS

--- src/widmy_Project/IPS/views.py
+++ src/widmy_Project/IPS/views.py
@@ -3,24 +3,30 @@ from .logic import IPS_logic as l
 from django.http import HttpResponse
 from django.core import serializers
 import json
-from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt, login_required
+from widmy_Project import getRole
 
 @csrf_exempt
+@login_required
 def IPSs_view(request):
-    if request.method=='GET':
-        id = request.GET.get("id",None)
-        if id:
-            IPS_dto = l.get_IPS(id)
-            ips = serializers.serialize('json',[IPS_dto,])
+    role = getRole(request)
+    if role == "Administrador":
+        if request.method=='GET':
+            id = request.GET.get("id",None)
+            if id:
+                IPS_dto = l.get_IPS(id)
+                ips = serializers.serialize('json',[IPS_dto,])
+                return HttpResponse(ips, 'application/json')
+            else:
+                IPSs_dto = l.get_IPSs()
+                IPSs = serializers.serialize('json', IPSs_dto)
+                return HttpResponse(IPSs, 'application/json')
+        if request.method == 'POST':
+            IPS_dto = l.create_IPS(json.loads(request.body))
+            ips = serializers.serialize('json', [IPS_dto, ])
             return HttpResponse(ips, 'application/json')
-        else:
-            IPSs_dto = l.get_IPSs()
-            IPSs = serializers.serialize('json', IPSs_dto)
-            return HttpResponse(IPSs, 'application/json')
-    if request.method == 'POST':
-        IPS_dto = l.create_IPS(json.loads(request.body))
-        ips = serializers.serialize('json', [IPS_dto, ])
-        return HttpResponse(ips, 'application/json')
+    else:
+        return HttpResponse("Unauthorized User")
 
 @csrf_exempt
 def IPS_view(request, pk):
