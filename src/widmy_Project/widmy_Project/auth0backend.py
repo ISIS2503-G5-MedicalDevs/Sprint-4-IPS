0db4a83ac61e32bf9c5a0176e8468b85c5d5120e
JohnSuarez20 <j.serratos@uniandes.edu.co>
2023-05-06 01:36:04 -0500

    .

--- src/widmy_Project/widmy_Project/auth0backend.py
+++ src/widmy_Project/widmy_Project/auth0backend.py
@@ -32,9 +32,9 @@ def getRole(request):
     user = request.user 
     auth0user = user.social_auth.get(provider="auth0")
     accessToken = auth0user.extra_data['access_token'] 
-    url = "https://isis2503-john-suarez20.auth0.com/userinfo" 
+    url = "https://isis2503-john-suarez20.us.auth0.com/userinfo" 
     headers = {'authorization': 'Bearer ' + accessToken}
     resp = requests.get(url, headers=headers)
     userinfo = resp.json()
-    role = userinfo['isis2503-john-suarez20.com/role']
+    role = userinfo['isis2503-john-suarez20.us.com/role']
     return (role)
