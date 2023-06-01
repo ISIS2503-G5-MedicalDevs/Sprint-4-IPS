22ce6b4165289fb633f0378eca150fcd8ef24116
msgalvan1207 <85647232+msgalvan1207@users.noreply.github.com>
2023-05-31 21:23:39 -0500

    corrección menor

--- src/widmy_Project/IPS/urls.py
+++ src/widmy_Project/IPS/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('',views.IPSs_view, name='IPSs_view'),
     path('<int:pk>', views.IPS_view, name='IPS_view'),
     path('test', views.IPS_test, name= 'IPS_test'),
-     path('test2', views.IPS_test2, name= 'IPS_test')
+     path('test2', views.IPS_test_2, name= 'IPS_test')
 ]
