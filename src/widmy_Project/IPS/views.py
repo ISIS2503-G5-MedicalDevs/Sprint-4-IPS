ec57f98a5b7f7953e7b17580686bc3e9269bd15a
msgalvan1207 <85647232+msgalvan1207@users.noreply.github.com>
2023-06-01 22:38:49 -0500

    Quitar auth0 (No funciono)

--- src/widmy_Project/IPS/views.py
+++ src/widmy_Project/IPS/views.py
@@ -8,10 +8,10 @@ from django.contrib.auth.decorators import login_required
 from widmy_Project.auth0backend import getRole
 import widmy_Project.validate as vl
 
-rolesValidos = ["Administrador", "AdministradorSistema"]
+#rolesValidos = ["Administrador", "AdministradorSistema"]
 
 @csrf_exempt
-@login_required
+#@login_required
 def IPSs_view(request):
     
     if request.method == 'POST':
@@ -19,42 +19,34 @@ def IPSs_view(request):
             ips = serializers.serialize('json', [IPS_dto, ])
             return HttpResponse(ips, 'application/json')
     
-    role = getRole(request)
-    if role in rolesValidos:
-        if request.method=='GET':
-            id = request.GET.get("id",None)
-            if id:
-                IPS_dto = l.get_IPS(id)
-                ips = serializers.serialize('json',[IPS_dto,])
-                return HttpResponse(ips, 'application/json')
-            else:
-                IPSs_dto = l.get_IPSs()
-                IPSs = serializers.serialize('json', IPSs_dto)
-                return HttpResponse(IPSs, 'application/json')
-    else:
-        return HttpResponse("Unauthorized User")
+    if request.method=='GET':
+        id = request.GET.get("id",None)
+        if id:
+            IPS_dto = l.get_IPS(id)
+            ips = serializers.serialize('json',[IPS_dto,])
+            return HttpResponse(ips, 'application/json')
+        else:
+            IPSs_dto = l.get_IPSs()
+            IPSs = serializers.serialize('json', IPSs_dto)
+            return HttpResponse(IPSs, 'application/json')
 
 @csrf_exempt
-@login_required
+#@login_required
 def IPS_view(request, pk):
-    role = getRole(request)
-    if role in rolesValidos:
-        if request.method == 'GET':
-            IPS_dto = l.get_IPS(pk)
-            ips = serializers.serialize('json', [IPS_dto,])
-            return HttpResponse(ips, 'application/json')
-        if request.method == 'PUT':
-            if not(vl.validar(json.loads(request.body))):
-                return HttpResponseBadRequest(HttpResponse("Error, invalid entry for update"))
-            IPS_dto = l.update_IPS(pk, json.loads(request.body))
-            ips = serializers.serialize('json', [IPS_dto,])
-            return HttpResponse(ips, 'application/json')
-        if request.method == 'DELETE':
-            IPS_dto = l.delete_IPS(pk)
-            ips = serializers.serialize('json', [IPS_dto,])
-            return HttpResponse(ips, 'application/json')
-    else:
-        return HttpResponse("Unauthorized User")
+    if request.method == 'GET':
+        IPS_dto = l.get_IPS(pk)
+        ips = serializers.serialize('json', [IPS_dto,])
+        return HttpResponse(ips, 'application/json')
+    if request.method == 'PUT':
+        if not(vl.validar(json.loads(request.body))):
+            return HttpResponseBadRequest(HttpResponse("Error, invalid entry for update"))
+        IPS_dto = l.update_IPS(pk, json.loads(request.body))
+        ips = serializers.serialize('json', [IPS_dto,])
+        return HttpResponse(ips, 'application/json')
+    if request.method == 'DELETE':
+        IPS_dto = l.delete_IPS(pk)
+        ips = serializers.serialize('json', [IPS_dto,])
+        return HttpResponse(ips, 'application/json')
 
 @csrf_exempt
 def IPS_test(request):
